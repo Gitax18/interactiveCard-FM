5e61e05f4e6bf104478f26ab8b5b9bf003d58ccf
gitanshu18 <lusifer0418@gmail.com>
2023-05-28 13:17:43 +0530

    adding files

---
+++ README.md
@@ -0,0 +1,78 @@
+# Frontend Mentor - Interactive card details form solution
+
+This is a solution to the [Interactive card details form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+
+## Overview
+The Interactive card details project is an frontend web project in which the task is to create an credit card and an form with some validations and when user clicks the confirm button the card details must updated to form user inputs. 
+
+### The challenge
+
+Users should be able to:
+
+- Fill in the form and see the card details update in real-time
+- Receive error messages when the form is submitted if:
+  - Any input field is empty
+  - The card number, expiry date, or CVC fields are in the wrong format
+- View the optimal layout depending on their device's screen size
+- See hover, active, and focus states for interactive elements on the page
+
+### Screenshot
+
+![screenshot](screenshot.jpg)
+
+### Links
+
+- Solution URL: [Solution page](https://your-solution-url.com)
+- Live Site URL: [Live Site](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- ES6 
+- DOM manipulation
+- Form validation
+
+
+### What I learned
+
+This is a great project to check multiple conditions while validating a form, the most difficult part of this project is to check the format of the input values.
+So, to check the format of the inputted string I have to learn regex, and it is really really tricky to grasp. I use regex to check that name must not contains any characters or any number and card number must not contains any character or letter.
+
+
+### Continued development
+
+The project is fully completed but When you enter the form confirm button, the details get updated and form section gets change to continue section and this section also has a button called 'continue' but nothings happens when you press this button so I want to create an banking dashboard so when user clicks on this button he will redirects to his dashboard where he/she can see all his details etc..
+
+
+### Useful resources
+
+- [regex resource](https://www.w3schools.com/jsref/jsref_obj_regexp.asp) - This website teach me regular expression. w3schools is best resource to learn anything and it has best examples and theory which makes everything easier to grasp.
+
+## Author
+
+- Name - Gitanshu Sankhla
+- Frontend Mentor - [@gitanshu18](https://www.frontendmentor.io/profile/gitanshu18)
+- Linkedin - [@gitanshu-sankhla](https://www.linkedin.com/in/gitanshu-sa)
+
+
---
+++ script.js
@@ -0,0 +1,152 @@
+'use strict'
+
+let cardNum, cardName, cardExp, cardCvc,cardholder,formNum,formDateMM,formDateYY,formCvc,btnCont,btnCof;
+// card details (on card image)
+cardNum = document.querySelector('#card-num');
+cardName = document.querySelector('#card-name');
+cardExp = document.querySelector('#card-exp');
+cardCvc = document.querySelector('#card-cvc');
+
+//  form inputs
+cardholder = document.querySelector('#cardholder-name');
+formNum = document.querySelector('#card-form-num');
+formDateMM = document.querySelector('#exp-date-mm');
+formDateYY = document.querySelector('#exp-date-yy');
+formCvc = document.querySelector('#form-cvc');
+
+// buttons
+btnCof = document.querySelector('#btn-cof');
+btnCont = document.querySelector('#btn-con');
+
+// screens
+let main = document.querySelector('.form')
+let cardCompleted = document.querySelector('.card-complete')
+
+// warning elements
+let cardBlank, cardWFormat, numBlank, numWFormat, dateBlank, dateWFormat, cvcBlank, cvcWFormat;
+cardBlank = document.querySelector('#no-name');
+cardWFormat = document.querySelector('#no-name-number');
+
+numBlank = document.querySelector('#no-card-number')
+numWFormat = document.querySelector('#no-card-letter')
+
+dateBlank = document.querySelector('#no-date')
+dateWFormat = document.querySelector('#wrong-date')
+
+cvcBlank = document.querySelector('#no-cvc')
+cvcWFormat = document.querySelector('#no-cvc-letter')
+
+
+
+// function to check validity 
+function checkFormValidity(){
+    let nameCorrect = false
+    let numCorrect = false
+    let dateCorrect = false
+    let cvcCorrect = false
+
+    /* the below code is for checking the format of cardHolder name in form */
+
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if ( cardholder.value.length == 0 ){           // cardholder name must not be empty string
+         cardBlank.classList.remove('hidden') 
+         cardholder.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+         
+         // -------------------------------------------   
+        } else if ((/\d/.test(cardholder.value))       // checking cardholder name not contains numbers or
+        || specialChars.test(cardholder.value) // checking cardholder name not contains special characters or
+        )// || cardholder.value.length == 0)       // cardholder name must not be empty string
+        {   cardBlank.classList.add('hidden')
+        cardWFormat.classList.remove('hidden')
+        cardholder.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+        // ---------------------------------------------
+    } else {
+        cardBlank.classList.add('hidden')
+        cardWFormat.classList.add('hidden')
+        cardholder.style.border = '2px solid hsl(270, 3%, 87%)'
+        nameCorrect = true
+    }   
+
+     /* the below code is for checking the format of card number in form */
+
+    if( formNum.value.length == 0 ) {              // checking card number must not be empty
+        numBlank.classList.remove('hidden')
+        formNum.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+        // ------------------------------------------------------------------------------------
+    } else if (!(formNum.value.length - formNum.value.replace(/\s/g, "").length === 3 ) //checking card number must contain 3 whitespace
+    || !(formNum.value.length === 19) // checking card number must be 19 characters long (16 numbers + 3 whitespace)
+    || !Boolean(Number(formNum.value.replace(/\s/g, "")))) // card number must not be an letter or special character
+    
+    {   numBlank.classList.add('hidden')
+        formNum.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+        numWFormat.classList.remove('hidden')
+        // ------------------------------------------------------------------------------------
+    }else{
+        formNum.style.border = '2px solid hsl(270, 3%, 87%)'
+        numBlank.classList.add('hidden')
+        numWFormat.classList.add('hidden')
+        numCorrect = true
+    }
+
+
+    /* the below code is for checking the format of card expiry date in form */ 
+    if (formDateMM.value.length == 0 || formDateYY.value.length == 0){ // year and must input must not be empty
+        dateBlank.classList.remove('hidden')
+        formDateMM.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+        formDateYY.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+        
+        // ------------------------------------------------ 
+    } else if (formDateMM.value < 1 // month must not be less than 1
+    || formDateMM.value > 12  // month must not be greater than 12 
+    || formDateYY.value < 23  // year must not be less 2023
+    || formDateYY > 53 // year must not be greater than 2053 (30 years limit)
+    || formDateMM.value.length !== 2 // month format must contains only 2 digits (eg: 04 correct, 4 incorrect)
+    || formDateYY.value.length !== 2    ){ // year format validity just like above
+        dateBlank.classList.add('hidden')
+        dateWFormat.classList.remove('hidden')
+        formDateMM.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+        formDateYY.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+        // ------------------------------------------------ 
+    }else{
+        dateBlank.classList.add('hidden')
+        dateWFormat.classList.add('hidden')
+        formDateMM.style.border = '2px solid  hsl(270, 3%, 87%)'
+        formDateYY.style.border = '2px solid  hsl(270, 3%, 87%)'
+        dateCorrect = true
+    }
+    
+    
+    if (formCvc.value.length == 0){ // checking form cvc text length is it 0 or not 
+        cvcBlank.classList.remove('hidden');
+        formCvc.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+    } else if (formCvc.value.length > 3  // form cvc must not be more than 3 digits
+    || formCvc.value.length < 3 ){ // form cvc must not be less than 3 digits
+        cvcBlank.classList.add('hidden');
+        cvcWFormat.classList.remove('hidden')
+        formCvc.style.border = '2px solid rgba(255, 0, 0, 0.75)'
+    } else{
+        cvcBlank.classList.add('hidden')
+        cvcWFormat.classList.add('hidden')
+        formCvc.style.border = '2px solid  hsl(270, 3%, 87%)'
+        cvcCorrect = true
+    }
+
+    // the 'detailsCorrect' is the boolean which determines is all the form inputs values are correct or not.
+    let detailsCorrect = nameCorrect && numCorrect && dateCorrect && cvcCorrect
+    return detailsCorrect
+}
+
+btnCof.addEventListener('click', function(){
+    const isDetailsCorrect = checkFormValidity()
+    if (isDetailsCorrect){
+        cardName.innerHTML = String(cardholder.value)
+        cardNum.innerHTML = String(formNum.value)
+        cardExp.innerHTML = `${formDateMM.value}/${formDateYY.value}`
+        cardCvc.innerHTML = String(formCvc.value)
+
+        cardCompleted.classList.remove('hidden')
+        main.classList.add('hidden')
+        // console.log(`${formDateMM.value}/${formDateYY}`)
+    }
+})
